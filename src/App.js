2d7ec73f1c8664ad1818ba9cebe59e259af65909
Nadiaofori <nadiaofori93@gmail.com>
2021-05-24 13:52:54 +0000

    app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 
-
-import { ChakraProvider } from "@chakra-ui/react"
 import AppRoutes from "./pages/approutes"
 function App() {
   return (
     <div className="App">
-      <ChakraProvider>
       <AppRoutes/>
-      </ChakraProvider>
     </div>
   );
 }
